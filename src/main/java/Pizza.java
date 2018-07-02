a40267176c6fc3da87003fd8d46a628dd3f2b395
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-07-02 20:05:02 +0200

    create Pizza.class

---
+++ src/main/java/Pizza.java
@@ -0,0 +1,18 @@
+public class Pizza {
+
+    private String dough;
+    private String topping;
+    private String sauce;
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+}
