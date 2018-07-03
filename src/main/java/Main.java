730999eba26b4d2a2791e84ef48035f4ae904d7c
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-07-03 18:52:40 +0200

    create Menu class, refactor Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        Waiter waiter = new Waiter();
-        boolean nextPizza = true;
-        while (nextPizza) {
-            System.out.println("Margherita: 1");
-            System.out.println("Hawaiian: 2");
-            System.out.println("Pepperoni: 3");
-            System.out.println("Exit: 0");
-            int pizzaOrder = scanner.nextInt();
-            switch (pizzaOrder) {
-                case 1:
-                    waiter.setPizzaBuilder(new MargheritaBuilder());
-                    System.out.println(waiter.getPizza());
-                    break;
-                case 2:
-                    waiter.setPizzaBuilder(new HawaiianBuilder());
-                    System.out.println(waiter.getPizza());
-                    break;
-                case 3:
-                    waiter.setPizzaBuilder(new PepperoniPizza());
-                    System.out.println(waiter.getPizza());
-                    break;
-                case 0:
-                    nextPizza = false;
-            }
-        }
+        Menu.start();
     }
 }
---
+++ src/main/java/Menu.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void start() {
+
+        Scanner scanner = new Scanner(System.in);
+        Waiter waiter = new Waiter();
+        boolean nextPizza = true;
+        while (nextPizza) {
+            System.out.println("Margherita: 1");
+            System.out.println("Hawaiian: 2");
+            System.out.println("Pepperoni: 3");
+            System.out.println("Exit: 0");
+            int pizzaOrder = scanner.nextInt();
+            switch (pizzaOrder) {
+                case 1:
+                    waiter.setPizzaBuilder(new MargheritaBuilder());
+                    System.out.println(waiter.getPizza());
+                    break;
+                case 2:
+                    waiter.setPizzaBuilder(new HawaiianBuilder());
+                    System.out.println(waiter.getPizza());
+                    break;
+                case 3:
+                    waiter.setPizzaBuilder(new PepperoniPizza());
+                    System.out.println(waiter.getPizza());
+                    break;
+                case 0:
+                    nextPizza = false;
+                    break;
+            }
+        }
+    }
+}
